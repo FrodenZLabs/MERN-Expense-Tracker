3db0150d3497105bec2b0a3b62c7e56fa1eb652f
Kibe Stevie <stevensonkibs55@gmail.com>
2025-05-27 20:30:07 +0300

    Added input and photo selector UI

---
+++ Frontend/src/components/inputs/ProfilePhotoSelector.jsx
@@ -0,0 +1,67 @@
+import { useRef, useState } from "react";
+import { LuUser, LuUpload, LuTrash } from "react-icons/lu";
+
+const ProfilePhotoSelector = ({ image, setImage }) => {
+  const inputRef = useRef(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImage(file);
+      const preview = URL.createObjectURL(file);
+      setPreviewUrl(preview);
+    }
+  };
+
+  const handleRemoveImage = () => {
+    setImage(null);
+    setPreviewUrl(null);
+  };
+
+  const onChooseFile = () => {
+    inputRef.current.click();
+  };
+
+  return (
+    <div className="flex justify-center mb-6">
+      <input
+        type="file"
+        ref={inputRef}
+        className="hidden"
+        accept="image/*"
+        onChange={handleImageChange}
+      />
+
+      {!image ? (
+        <div className="w-20 h-20 flex items-center justify-center relative rounded-full bg-purple-100">
+          <LuUser />
+
+          <button
+            onClick={onChooseFile}
+            className="w-8 h-8 flex items-center justify-center bg-primary text-white rounded-full absolute -bottom-1 -right-1"
+          >
+            <LuUpload />
+          </button>
+        </div>
+      ) : (
+        <div className="relative">
+          <img
+            src={previewUrl}
+            alt="Profile photo"
+            className="w-20 h-20 rounded-full object-cover"
+          />
+
+          <button
+            onClick={handleRemoveImage}
+            className="w-8 h-8 flex items-center justify-center bg-primary text-white rounded-full absolute -bottom-1 -right-1"
+          >
+            <LuTrash />
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProfilePhotoSelector;
