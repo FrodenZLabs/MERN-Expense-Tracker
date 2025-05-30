bff70ad401d94c3843d3923f4938c67329af62a9
Kibe Stevie <stevensonkibs55@gmail.com>
2025-05-31 02:05:11 +0300

    Updated app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,15 +6,13 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Income from "./pages/Dashboard/Income";
 import Expense from "./pages/Dashboard/Expense";
-import Root from "./utils/root";
 
 const App = () => {
   return (
     <BrowserRouter>
       <ToastContainer />
       <Routes>
-        <Route path="/" element={<Root />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/dashboard" element={<Home />} />
         <Route path="/income" element={<Income />} />
