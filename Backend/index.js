cc9ca44c90a7521ed48b113de7bfb83c2266fe76
Kibe Stevie <stevensonkibs55@gmail.com>
2025-05-31 00:48:14 +0300

    Added config for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import error from "./config/error.js";
@@ -27,6 +28,9 @@ connectDB();
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/expense", expenseRoutes);
 app.use("/api/v1/income", incomeRoutes);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
