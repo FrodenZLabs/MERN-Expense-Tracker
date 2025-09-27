fddc5eced7d5e7d81bc11a385059dff926071307
kibestevie <frodenzlabs@gmail.com>
2025-09-27 19:39:57 +0300

    Updated the url config for production

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "https://mern-expense-tracker-f9le.onrender.com";
 
 export const loginUser = async (email, password) => {
   try {
