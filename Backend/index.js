54672d0b40261421ee039ffb26211676a234f645
kibestevie <frodenzlabs@gmail.com>
2025-09-27 19:23:40 +0300

    Updated cors config 🛠️

--- Backend/index.js
+++ Backend/index.js
@@ -12,9 +12,20 @@ dotenv.config();
 
 const app = express();
 
+// CORS (allow frontend)
+const allowedOrigins = [
+  "http://localhost:5173", 
+  process.env.FRONTEND_URL
+];
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
